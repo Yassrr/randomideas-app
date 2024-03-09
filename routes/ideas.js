4d1226c9c79bf9d03abe98a66444da96bd8de0ae
Yassrr <yasser.khelassi1@gmail.com>
2024-03-09 16:22:57 +0100

    connecting with mongoDb and create CRUD operations

---
+++ routes/ideas.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const router = express.Router();
+
+const ideas = [
+  {
+    id: 1,
+    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
+    tag: 'Technology',
+    username: 'TonyStark',
+    date: '2022-01-02',
+  },
+  {
+    id: 2,
+    text: 'Milk cartons that turn a different color the older that your milk is getting',
+    tag: 'Inventions',
+    username: 'SteveRogers',
+    date: '2022-01-02',
+  },
+  {
+    id: 3,
+    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
+    tag: 'Software',
+    username: 'BruceBanner',
+    date: '2022-01-02',
+  },
+];
+  
+// GET All facutre
+router.get('/', (req, res) => {
+  res.json({succes: true, data: ideas});
+});
+
+// GET single idea 
+router.get('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if(!idea) {
+    res
+      .status(404)
+      .json({succes: false, error: 'Ressource not found'});
+  }
+  res.json({succes: true, data: idea});
+});
+
+
+// add an idea
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+
+  ideas.push(idea);
+
+  res.json({ success: true, data: idea });
+});
+
+// update an idea
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if(!idea) {
+    res
+      .status(404)
+      .json({succes: false, error: 'Ressource not found'});
+  }
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({succes: true, data: idea});
+});
+
+// delete an idea
+router.delete('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if(!idea) {
+    res
+      .status(404)
+      .json({succes: false, error: 'Ressource not found'});
+  }
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1);
+
+  res.json({succes: true, data: {}});
+});
+
+module.exports = router;
